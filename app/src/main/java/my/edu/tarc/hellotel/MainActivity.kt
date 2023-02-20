3e5f15cd2b8854a48354d76e1dc67cef85cd481d
Eng Lip <englip455@gmail.com>
2023-02-20 21:30:27 +0800

    add switch

--- app/src/main/java/my/edu/tarc/hellotel/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/hellotel/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
 
 //Instant name: Type / Class Name
@@ -25,16 +26,33 @@ class MainActivity : AppCompatActivity() {
         val textViewName: TextView = findViewById(R.id.textViewName)
         val buttonShow: Button = findViewById(R.id.buttonShow)
         val buttonHide = findViewById<Button>(R.id.buttonHide) //casting to Button (not preferred)
+        val switchShow: Switch = findViewById(R.id.switchShow)
+
 
         //Insert event handler
         buttonShow.setOnClickListener{
+            switchShow.isChecked = true
             imageViewQR.visibility = View.VISIBLE
             textViewName.visibility = View.VISIBLE
         }
 
         buttonHide.setOnClickListener {
+            switchShow.isChecked = false
             imageViewQR.visibility = View.INVISIBLE
             textViewName.visibility = View.INVISIBLE
         }
+
+        switchShow.setOnClickListener {
+            if (switchShow.isChecked){
+                imageViewQR.visibility = View.VISIBLE
+                textViewName.visibility = View.VISIBLE
+            }
+            else{
+                imageViewQR.visibility = View.INVISIBLE
+                textViewName.visibility = View.INVISIBLE
+            }
+        }
+
+
     }
 }
